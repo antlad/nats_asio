9304d1cbf463ff98130893ab0639d7cb10dc645a
Vladislav Troinich <antlad@icloud.com>
2019-10-11 13:07:40 +0300

    Adding ssl

--- src/parser.hpp
+++ src/parser.hpp
@@ -9,22 +9,23 @@ namespace nats_asio {
 
 struct parser_observer
 {
-    virtual ~parser_observer() = default;
+	virtual ~parser_observer() = default;
 
-    virtual void on_ping(ctx c) = 0;
+	virtual void on_ping(ctx c) = 0;
 
-    virtual void on_pong(ctx c) = 0;
+	virtual void on_pong(ctx c) = 0;
 
-    virtual void on_ok(ctx c) = 0;
+	virtual void on_ok(ctx c) = 0;
 
-    virtual void on_error(string_view err, ctx c) = 0;
+	virtual void on_error(string_view err, ctx c) = 0;
 
-    virtual void on_info(string_view info, ctx c) = 0;
+	virtual void on_info(string_view info, ctx c) = 0;
 
-    virtual void on_message(string_view subject, string_view sid, optional<string_view> reply_to, std::size_t n, ctx c) = 0;
+	virtual void on_message(
+		string_view subject, string_view sid, optional<string_view> reply_to, std::size_t n, ctx c)
+		= 0;
 
-    virtual void consumed(std::size_t n) = 0;
-
+	virtual void consumed(std::size_t n) = 0;
 };
 
 status parse_header(std::string& header, std::istream& is, parser_observer* observer, ctx c);
---
+++ src/socket.hpp
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <nats_asio/defs.hpp>
+
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/read.hpp>
+#include <boost/asio/read_until.hpp>
+
+namespace nats_asio {
+
+using boost::asio::ip::tcp;
+namespace ssl = boost::asio::ssl;
+
+constexpr auto sep = "\r\n";
+
+class uni_socket
+{
+public:
+	uni_socket(ssl::context& ctx, aio& io)
+		: m_socket_ssl(io, ctx)
+		, m_socket(io)
+		, m_use_ssl(false)
+	{}
+
+	void async_connect(std::string address, uint16_t port, ctx c)
+	{
+		if (m_use_ssl) {
+			m_socket_ssl.lowest_layer().async_connect(
+				boost::asio::ip::tcp::endpoint(boost::asio::ip::make_address(address), port), c);
+		} else {
+			m_socket.async_connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::make_address(
+																	  address),
+																  port),
+								   c);
+		}
+	}
+
+	void set_use_ssl(bool use_ssl) { m_use_ssl = use_ssl; }
+
+	template<class Buf>
+	void async_read_until(Buf& buf, ctx c)
+	{
+		if (m_use_ssl) {
+			boost::asio::async_read_until(m_socket_ssl, buf, sep, c);
+		} else {
+			boost::asio::async_read_until(m_socket, buf, sep, c);
+		}
+	}
+
+	template<class Buf, class Transfer>
+	void async_read(Buf& buf, const Transfer& until, ctx c)
+	{
+		if (m_use_ssl) {
+			boost::asio::async_read(m_socket_ssl, buf, until, c);
+		} else {
+			boost::asio::async_read(m_socket, buf, until, c);
+		}
+	}
+
+	template<class Buf, class Transfer>
+	void async_write(const Buf& buf, const Transfer& until, ctx c)
+	{
+		if (m_use_ssl) {
+			boost::asio::async_write(m_socket_ssl, buf, until, c);
+		} else {
+			boost::asio::async_write(m_socket, buf, until, c);
+		}
+	}
+
+	void close(boost::system::error_code& ec)
+	{
+		if (m_use_ssl) {
+			m_socket_ssl.lowest_layer().close(ec);
+		} else {
+			m_socket.close(ec);
+		}
+	}
+
+private:
+	boost::asio::ssl::stream<boost::asio::ip::tcp::socket> m_socket_ssl;
+	boost::asio::ip::tcp::socket m_socket;
+	bool m_use_ssl;
+};
+
+} // namespace nats_asio
