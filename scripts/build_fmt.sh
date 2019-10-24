8d84e2dc86c4eb804d1472ff5e74bc0461380dcd
Vladislav Troinich <antlad@icloud.com>
2019-10-25 00:24:03 +0300

    Add cget script for cmake projects

--- scripts/build_fmt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pushd ../../fmt-6.0.0
-mkdir -p build
-pushd build
-cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_TOOLCHAIN_FILE="${PWD}/../../prefix/toolchain.txt" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PWD}/../../prefix" -GNinja ..
-ninja install
-popd
-popd
--- scripts/build_spdlog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pushd ../../spdlog-1.3.1
-mkdir -p build
-pushd build
-cmake -DBUILD_SHARED_LIBS=ON -DSPDLOG_BUILD_BENCH=OFF -DSPDLOG_BUILD_TESTS=OFF -DSPDLOG_FMT_EXTERNAL=ON -DSPDLOG_BUILD_EXAMPLES=OFF -DCMAKE_TOOLCHAIN_FILE="${PWD}/../../prefix/toolchain.txt" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PWD}/../../prefix" -GNinja ..
-ninja install
-popd
-popd
