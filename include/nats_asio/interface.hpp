9f087d6208826fb75ae30cde9a97005d595196ff
Vladislav Troinich <antlad@icloud.com>
2019-11-15 13:48:28 +0300

    Move deps to conan

--- include/nats_asio/interface.hpp
+++ include/nats_asio/interface.hpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2019 Vladislav Troinich
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                                                              copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+*/
+
 #pragma once
 
 #include <fmt/format.h>
@@ -6,11 +30,13 @@
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/spawn.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/deadline_timer.hpp>
 
 #include <boost/concept/detail/general.hpp>
 #include <boost/concept/detail/general.hpp>
 #include <boost/optional.hpp>
 #include <boost/utility/string_view.hpp>
+
 
 #include <stdexcept>
 #include <memory>
@@ -46,7 +72,10 @@ class status {
 public:
     status() = default;
 
-    status(const std::string& error);
+    status(const std::string& error)
+        : m_error(error)
+    {
+    }
 
     template <typename S, typename... Args, typename Char = fmt::char_t<S>>
     status(const S& format_str, Args && ... args)
--- scripts/build_prefix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export CGET_PREFIX=`realpath $PWD/../../prefix2`
-cget init --toolchain ${CGET_PREFIX}/toolchain.cmake
-cget install pfultz2/cget-recipes
-cget install fmtlib/fmt@6.0.0
-cget install gabime/spdlog@v1.4.2
-cget install google/googletest@release-1.10.0
-cget install jarro2783/cxxopts@v2.2.0
-cget install nlohmann/json@v3.7.0
