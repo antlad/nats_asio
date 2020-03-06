bb149c96060026f8dc66168716a03b808a5c2ec1
antlad <antlad@users.noreply.github.com>
2020-03-06 16:41:17 +0300

    Remove remote due conan center update

--- config_conan.sh
+++ config_conan.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 conan profile new default --detect || true
 conan profile update settings.compiler.libcxx=libstdc++11 default || true
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan True || true
-conan remote add conan-community https://api.bintray.com/conan/conan-community/conan True || true
 conan remote add inexorgame https://api.bintray.com/conan/inexorgame/inexor-conan True || true
